d38d80e787ce27c0fdcf6c5ce390e669f00ae7d4
jmisina <jmisina02@gmail.com>
2025-02-16 14:48:19 +0100

    User Microservice init
    Project initialization, users microservice

---
+++ UserMsvc/src/main/java/com/example/UserMsvc/UserMsvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserMsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsvcApplication.class, args);
+	}
+
+}
